edd32866dfe4d3673ad712269ada3e170c252f95
Tim Geerts <tim.geerts1@gmail.com>
2018-12-11 14:58:10 +0100

    boss guides v2, firebase integration

--- src/modules/guides/index.ts
+++ src/modules/guides/index.ts
@@ -1,9 +1,6 @@
 import Discord, { RichEmbed } from "discord.js";
+import fetch from "node-fetch";
 import { ResponderBotModule } from "../responderBotModule";
-
-interface IGuide {
-  raids: IRaid[];
-}
 
 interface IRaid {
   name: string;
@@ -35,12 +32,13 @@ interface IGuideCommand {
 export default class GuidesModule extends ResponderBotModule
 {
     private static readonly MODULE_NAME = "Raid Guides";
-    private readonly guides: IGuide;
+    private readonly FIREBASE_URL = "https://greggs-d6c7a.firebaseio.com/";
+    private raids: IRaid[];
 
     constructor(client: Discord.Client)
     {
-        super(client, GuidesModule.MODULE_NAME);
-        this.guides = require("../../../resources/bossGuides.json");
+      super(client, GuidesModule.MODULE_NAME);
+      this.raids = new Array<IRaid>();
     }
 
     public getHelpText()
@@ -48,7 +46,7 @@ export default class GuidesModule extends ResponderBotModule
       return {
         content: `__Description__: Fetch boss guides like a BOSS.\n__Usage__: \`!guide <raid> <boss>\``,
         moduleName: GuidesModule.MODULE_NAME,
-    };
+      };
     }
 
     protected process(message: Discord.Message): string
@@ -65,44 +63,7 @@ export default class GuidesModule extends ResponderBotModule
           boss: args.length === 3 ? args[2] : args[1],
           raid: args.length === 3 ? args[1] : "",
         };
-        const guide = this.lookupBossGuide(cmd);
-        if (guide)
-        {
-          const embed = new RichEmbed()
-          .setTitle(`__${guide.name} Mythic - ${guide.raid}__`)
-          .setColor(0xff0000);
-
-          if (guide.thumbnail)
-          {
-            embed.setThumbnail(guide.thumbnail);
-          }
-          if (guide.description)
-          {
-            embed.setDescription(guide.description);
-          }
-          if (guide.wowhead)
-          {
-            embed.addField("Wowhead", guide.wowhead, true);
-          }
-          if (guide.youtube)
-          {
-            embed.addField("Youtube", guide.youtube);
-          }
-          if (guide.extra && guide.extra.length)
-          {
-            guide.extra.forEach((e) =>
-            {
-              embed.addField(e.name, e.content);
-            });
-          }
-          message.reply(embed);
-        }
-        else
-        {
-          let msg = `No guide was found for a boss named "${cmd.boss}"`;
-          msg += cmd.raid ? `in a raid named "${cmd.raid}."` : `.`;
-          message.reply(`${msg}\nBut here, have a cookie :cookie:.`);
-        }
+        this.handleCommand(cmd, message);
       }
       return "";
     }
@@ -111,6 +72,69 @@ export default class GuidesModule extends ResponderBotModule
     {
       const command = content.replace(/ .*/, "").replace(/!/, "");
       return (command.toLowerCase() === "guide");
+    }
+
+    private handleCommand(cmd: IGuideCommand, message: Discord.Message)
+    {
+      const URL = `${this.FIREBASE_URL}raids.json`;
+      fetch(URL).then((r) =>
+      {
+        if (r.ok)
+        {
+            return r.json();
+        }
+        throw new Error(r.statusText);
+      }).then((raids: IRaid[]) =>
+      {
+        this.raids = raids;
+        const guide = this.lookupBossGuide(cmd);
+        if (!guide)
+        {
+          let msg = `No guide was found for a boss named "${cmd.boss}"`;
+          msg += cmd.raid ? `in a raid named "${cmd.raid}."` : `.`;
+          msg += `\nBut here, have a cookie :cookie:.`;
+          throw new Error(msg);
+        }
+        else
+        {
+          this.reply(guide, message);
+        }
+      }).catch((err: Error) =>
+      {
+        message.reply(`Sorry, I had some trouble fetching that information.\n\n${err.message}`);
+      });
+    }
+
+    private reply(guide: IBoss, message: Discord.Message)
+    {
+      const embed = new RichEmbed()
+      .setTitle(`__${guide.name} Mythic - ${guide.raid}__`)
+      .setColor(0xff0000);
+
+      if (guide.thumbnail)
+      {
+        embed.setThumbnail(guide.thumbnail);
+      }
+      if (guide.description)
+      {
+        embed.setDescription(guide.description);
+      }
+      if (guide.wowhead)
+      {
+        embed.addField("Wowhead", guide.wowhead, true);
+      }
+      if (guide.youtube)
+      {
+        embed.addField("Youtube", guide.youtube);
+      }
+      if (guide.extra && guide.extra.length)
+      {
+        guide.extra.forEach((e) =>
+        {
+          embed.addField(e.name, e.content);
+        });
+      }
+      message.reply(embed);
     }
 
     private lookupBossGuide(command: IGuideCommand): IBoss | undefined
@@ -133,7 +157,7 @@ export default class GuidesModule extends ResponderBotModule
 
     private lookupRaid(name: string): IRaid | undefined
     {
-      return this.guides.raids.find((r) => r.tags.map((l) => l.toLowerCase()).indexOf(name.toLowerCase()) > -1);
+      return this.raids.find((r) => r.tags.map((l) => l.toLowerCase()).indexOf(name.toLowerCase()) > -1);
     }
 
     private lookupBoss(name: string, raid?: IRaid): IBoss | undefined
@@ -142,8 +166,7 @@ export default class GuidesModule extends ResponderBotModule
       {
         return this.lookupBossForRaid(raid, name);
       }
-      const knownraids = this.guides.raids;
-      for (const r of knownraids)
+      for (const r of this.raids)
       {
         const boss = this.lookupBossForRaid(r, name);
         if (boss)
@@ -156,11 +179,15 @@ export default class GuidesModule extends ResponderBotModule
 
     private lookupBossForRaid(raid: IRaid, name: string): IBoss | undefined
     {
-      const boss = raid.bosses.find((b) => b.tags.map((l) => l.toLowerCase()).indexOf(name.toLowerCase()) > -1);
-      if (boss)
+      if (raid.bosses)
       {
-        boss.raid = raid.name;
+        const boss = raid.bosses.find((b) => b.tags.map((l) => l.toLowerCase()).indexOf(name.toLowerCase()) > -1);
+        if (boss)
+        {
+          boss.raid = raid.name;
+        }
+        return boss;
       }
-      return boss;
+      return undefined;
     }
 }
