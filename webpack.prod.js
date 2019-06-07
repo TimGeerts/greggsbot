d5f6fa062e2be7b1b80122741c1a83917d1e4645
Tim Geerts <tim.geerts1@gmail.com>
2019-06-08 01:28:50 +0200

    prod script include env file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,14 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const envLoader = require('dotenv-webpack');
 
 module.exports = merge(common, {
     mode: 'production',
-    optimization: { minimize: false }
+    optimization: { minimize: false },
+    plugins: [
+        new envLoader({
+            path: './environment.env',
+            systemvars: true
+        })
+    ]
 });
