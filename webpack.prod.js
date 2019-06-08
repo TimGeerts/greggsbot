c1753f2f7174321691a02906794b08f71f9b7bc6
Tim Geerts <tim.geerts1@gmail.com>
2019-06-08 15:19:02 +0200

    removed some unneeded files, updated gitignore

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,7 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
-const envLoader = require('dotenv-webpack');
 
 module.exports = merge(common, {
     mode: 'production',
-    optimization: { minimize: false },
-    plugins: [
-        new envLoader({
-            path: './environment.env',
-            systemvars: true
-        })
-    ]
+    optimization: { minimize: false }
 });
